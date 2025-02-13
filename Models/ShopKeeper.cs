4f21d9b069d3d7961c9d2f7c48453671da21e336
Mezbah15 <mezba0044@gmail.com>
2025-02-13 14:02:32 +0600

    UI Final

---
+++ Models/ShopKeeper.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace hki_2025_registration.Models
+{
+    public class ShopKeeper
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [RegularExpression(@"^\d{11}$", ErrorMessage = "Contact number must be exactly 11 digits.")]
+        public string Contact { get; set; }
+        public string Address { get; set; }
+        public string? Description { get; set; }
+    }
+}
